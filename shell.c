b9b0992c706c182d47b36b065b28c413871da37b
JUCORTESA <984@holbertonschool.com>
2019-08-22 14:05:28 +0000

    [FEATURE] enviromental variable

--- shell.c
+++ shell.c
@@ -3,12 +3,13 @@
  * main - execute a simple shell
  * Return: nothing
  */
-int main(int ac, char **av)
+int main(int ac, char **av, char **env)
 {
 	char *buffer, **buff = NULL;
 	ssize_t c, flag = 0;
 	ssize_t *p = &c;
 	pid_t child_pid;
+	int i;
 
 	if (ac != 1)
 	{
@@ -32,6 +33,13 @@ conti:
 	if (child_pid == 0)
 	{
 		execve(buff[0], buff, NULL);
+		i = 0;
+		while (env[i] != '\0')
+		{
+			write(STDOUT_FILENO, env[i], strlen(env[i]));
+			write(STDOUT_FILENO, "\n", 1);
+			i++;
+		}
 		exit(0);
 	}
 	else
